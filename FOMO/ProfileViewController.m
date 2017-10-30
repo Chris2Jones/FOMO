c52d62e6c4ca37723a7c84073c18cbcada0a8a75
Kingsley Chu <kingsleychu0730@hotmail.com>
2017-10-30 13:33:46 -0400

    Added tableview controller

--- FOMO/ProfileViewController.m
+++ FOMO/ProfileViewController.m
@@ -10,20 +10,81 @@
 
 @interface ProfileViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *header;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *content;
+
 @end
 
 @implementation ProfileViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupHeader];
+    [self setupTableview];
+    
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupHeader{
+    self.header.clipsToBounds = NO;
+    self.header.layer.masksToBounds = NO;
+    self.header.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.header.layer.shadowOpacity = 0.5f;
+    self.header.layer.shadowRadius = 1.5f;
+    self.header.layer.shadowOffset = CGSizeMake(0.0f, 2.0f);
+    [self.view bringSubviewToFront:self.header];
 }
 
+- (void)setupTableview {
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.content = @[@"Monday", @"Tuesday", @"Wednesday",@"Thursday",@"Friday",@"Saturday", @"Sunday" ];
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if (section == 0){
+        UIView *v = [UIView new];
+        [v setBackgroundColor:[UIColor greenColor]];
+        return v;
+    } else {
+        UIView *v = [UIView new];
+        [v setBackgroundColor:[UIColor blueColor]];
+        return v;
+    }
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if (section == 0){
+        return 140;
+    } else {
+        return 70;
+    }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 70;
+}
+
+-  (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.content.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"cellIdentifier";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if(cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+        
+    }
+    cell.textLabel.text =  [self.content objectAtIndex:indexPath.row];
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
+}
 
 @end
